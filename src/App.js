f8d170807a38f1ddd7b14e1b5d7b42aa8db8d9e8
nomboibrahim <nomboibrahim87@gmail.com>
2022-03-21 08:33:14 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import profileCard from "./component/ProfileCards";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, { Component } from 'react'
+class App  extends Component {
+  constructor() {
+    super();
+    
+    this,handleclick =this.handleclick.bind(this)
 
-export default App;
+    this
+      <div>App</div>
+    )
+
+
+
+
+
+
+
+
+
+setTimeout(async() => {
+  let resp =await fetch("/writers.json");
+  let result
+
+
---
+++ src/profileCard.js
@@ -0,0 +1,11 @@
+import React from'react
+
+function profileCard({rwiter}) {
+    return (
+        <div className='card'
+        <img src= {`images/${writer.avatar}`.png
+        height="300px" width="300px" alt={writer.img}
+    )
+}
+
+< className='textGroup'>
